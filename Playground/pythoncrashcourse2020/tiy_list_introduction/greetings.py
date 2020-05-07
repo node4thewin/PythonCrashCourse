d5f823ab69a09f63a97f43049a9cbc61c8f28c5f
node4thewin <node4thewin@users.noreply.github.com>
2020-05-07 19:22:27 -0400

    Create greetings.py

---
+++ Playground/pythoncrashcourse2020/tiy_list_introduction/greetings.py
@@ -0,0 +1,17 @@
+# 3-2 GREETINGS
+
+# Start with the list you used in 3-1 (names.py), but instead of just printing each person's
+# name, print a message to them. The text of each message should be the same, but each
+# message should be personalized with the person's name.
+
+names = ['callie', 'jac', 'james', 'peter', 'sydney', 'jake', 'matt']
+
+message = " is my best friend."
+
+print(f'{names[0].title()}{message}')
+print(f'{names[1].title()}{message}')
+print(f'{names[2].title()}{message}')
+print(f'{names[3].title()}{message}')
+print(f'{names[4].title()}{message}')
+print(f'{names[5].title()}{message}')
+print(f'{names[6].title()}{message}')
