ba550e20365d1a6a6d10e708719d759f47589632
node4thewin <node4thewin@users.noreply.github.com>
2020-05-07 19:22:22 -0400

    Update names.py

--- Playground/pythoncrashcourse2020/tiy_list_introduction/names.py
+++ Playground/pythoncrashcourse2020/tiy_list_introduction/names.py
@@ -3,3 +3,12 @@
 # Store the names of a few of your friends in a list called "names". Print each person's
 # name by accessing each element in the list, one at a time.
 
+names = ['callie', 'jac', 'james', 'peter', 'sydney', 'jake', 'matt']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[6])
