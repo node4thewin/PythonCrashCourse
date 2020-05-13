e899c51dcabb1a5667f2c4661212630e63eb9448
node4thewin <node4thewin@users.noreply.github.com>
2020-05-12 23:46:26 -0400

    changing guest list updated

--- Playground/pythoncrashcourse2020/lists/tiy_lists/changing_guest_list.py
+++ Playground/pythoncrashcourse2020/lists/tiy_lists/changing_guest_list.py
@@ -11,7 +11,18 @@ guest_list = ['michael jordan', 'tiger woods', 'kobe bryant', 'mudge', 'callie b
 remove_guest = guest_list.pop(4)
 print(f"Unfortunately, we have just received word that {remove_guest.title()} has rescinded their invitation. Andrew fucked up.")
 
+# Modify your list, replacing the name of the guest who can't make it with the name of the
+# new person you're inviting.
+
 guest_list.insert(4, 'susan barron')
 print(f"{guest_list[4].title()} has decided to take the place of {remove_guest.title()}.")
+print(guest_list)
 
+# Print a second set of invitation messages, one for each person who is still in your list.
+
+print(f"Hello! This is a confirmation for {guest_list[0].title()}. Please bring a copy of this message to dinner on the eve of May 16th, 2020.")
+print(f"Hello! This is a confirmation for {guest_list[1].title()}. Please bring a copy of this message to dinner on the eve of May 16th, 2020.")
+print(f"Hello! This is a confirmation for {guest_list[2].title()}. Please bring a copy of this message to dinner on the eve of May 16th, 2020.")
+print(f"Hello! This is a confirmation for {guest_list[3].title()}. Please bring a copy of this message to dinner on the eve of May 16th, 2020.")
+print(f"Hello! This is a confirmation for {guest_list[4].title()}. Please bring a copy of this message to dinner on the eve of May 16th, 2020.")
 
