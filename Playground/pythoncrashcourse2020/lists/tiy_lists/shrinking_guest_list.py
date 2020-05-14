3e034c97ae9d2963920c88eefbbcb2ed14710326
node4thewin <node4thewin@users.noreply.github.com>
2020-05-13 21:35:08 -0400

    cabin coding 5/13

---
+++ Playground/pythoncrashcourse2020/lists/tiy_lists/shrinking_guest_list.py
@@ -0,0 +1,15 @@
+# 3-7 Shrinking Guest List
+# You just found out that your new dinner table won't arrive in time for the dinner, and you
+# have space for only two guests.
+
+# Start with your program from 3-6. Add a new line that prints a message saying that you can
+# invite only two people for dinner.
+
+guest_list = ['michael buble', 'michael jordan', 'tiger woods', 'otis barron', 'kobe bryant', 'mudge', 'callie barron', 'mike barron sr.']
+message = "Unfortunately I have just received word that I can only invite two people to dinner this evening. Please accept my apology. The following people will still be invited..."
+print(message)
+
+# Use pop() to remove guests from your list one at a time until only two names remain
+# in your list. Each time you pop a name from your list, print a message to that person letting
+# them know you're sorry you can't invite them to dinner.
+
