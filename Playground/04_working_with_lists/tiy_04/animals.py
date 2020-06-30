7be474b077dc41969934a7c8098264a982de231e
node4thewin <node4thewin@users.noreply.github.com>
2020-06-30 02:36:33 -0400

    changed file structure

---
+++ Playground/04_working_with_lists/tiy_04/animals.py
@@ -0,0 +1,9 @@
+# 4-2 ANIMALS
+# Think of at least three different kinds of animals that have a common characteristic. Store the names of these animals in a list, and then use them to print out the name of each animal
+
+big_cats = ['jaguar', 'leopard', 'mountain lion', 'tiger', 'cheetah', 'lion']
+for big_cat in big_cats:
+# Modify your program to print a statement about each animal. Something like "A dog would make a great pet."
+  print(f"A {big_cat.title()} would make a great pet in any medievil household.")
+# Add a line at the end of your program stating what these animals have in common. You could print a sentence such as "Any of these animals would make a great pet!"
+print('\nEach of these animals are known as "Big Cats" and would actually make a terrible housepet.')
--- Playground/pythoncrashcourse2020/ch4_working_with_lists/tiy_ch4/pizzas.py
+++
@@ -1,7 +0,0 @@
-# 4-1 TRY IT YOURSELF - PIZZAS
-
-# Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a 'for' loop to print the name of each pizza.
-
-pizza = 
-
-# 1. Modify your 'for' loop to print a sentence using the name of the pizza instead of printing   just the name of the pizza. For each pizza you should have one line of output containing a simple statement like "I like pepperoni pizza."
