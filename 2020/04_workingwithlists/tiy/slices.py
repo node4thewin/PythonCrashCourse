37808ab5fc9a07fbdfa684df367d53caa724b744
node4thewin <node4thewin@users.noreply.github.com>
2020-07-03 16:50:23 -0400

    updated file structure and new projects

---
+++ 2020/04_workingwithlists/tiy/slices.py
@@ -0,0 +1,16 @@
+# 4-10 SLICES
+
+# Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+
+# 1. Print the message "The first three items in the list are:". Then use a slice to print the first three items from that program's list.
+pizzas = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'buffalo chicken']
+print("The first three items in the list are:")
+print(pizzas[0:3])
+
+# 2. Print the message "Three items from the middle of the list are:". Use a slie to print three items from the middle of the list.
+print("\nThree items from the middle of the list are:")
+print(pizzas[1:4])
+
+# Print the message "The last three items in the list are:". Use a slice to print three items from the middle of the list.
+print("\nThe last three items in the list are:")
+print(pizzas[:-4])
--- 2020/04_workingwithlists/working with part of a list/copying_a_list.py
+++ 2020/04_workingwithlists/working with part of a list/copying_a_list.py
@@ -12,7 +12,7 @@ callie_foods = my_foods[:]
 print("My favorite foods are:")
 print(my_foods)
 
-print("\nMy friend's favorite foods are:")
+print("\nMy girlfriend's favorite foods are:")
 print(callie_foods)
 
 # At line 9 we make a list of the foods we like called 'my_foods'. At line 10 we make a new list called friend_foods. We make a copy of my_foods by asking for a slice of my_foods without specifying any indices and store the copy in friend_foods. When we print each list, we see that they both contain the same foods.
@@ -22,8 +22,29 @@ print(callie_foods)
 my_foods.append('bacon cheeseburgers')
 callie_foods.append('crab cakes')
 
+print("\n\tThis is the example that shows two separate lists...")
 print("\nMy favorite foods are:")
 print(my_foods)
 
 print("\nMy friend's favorite foods are:")
 print(callie_foods)
+
+# At line 10 we copy the original items in my_foods to the new list callie_foods, as we did in the previous example. Next, we add a new food to each list: at line 22 we add 'bacon cheeseburgers' to 'my_foods', and at line 23 we add 'crab cakes' to friend_foods. We then print the two lists to see whether each of these foods is in the appropriate list.
+
+# The output shows that 'bacon cheeseburgers' now appears in our list of favorite foods but 'crab cakes' doesn't. We also see that 'crab cakes' appears in "callie_foods" but 'bacons cheeseburgers' does not. If we had simply set callie_foods equal to my_foods, we would not produce two separate lists. For example, here's what happens when you try to copy a list without using a slice:
+
+# THIS DOESNT WORK:
+callie_foods = my_foods
+
+my_foods.append('breakfast sandwiches')
+callie_foods.append('french fries')
+
+print("\n\tThis is the example that doesnt work...")
+print("\nMy favorite foods are:")
+print(my_foods)
+
+print("\nCallie's favorite foods are:")
+print(callie_foods)
+
+# You can see now that they are just carbon copies, not separate lists.
+
