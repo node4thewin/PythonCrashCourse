fa4dcc5e308946bd02622d31f5894a1e13df6f1d
node4thewin <node4thewin@users.noreply.github.com>
2020-07-18 21:51:28 -0400

    moved tiy in 05

--- 2020/05_if statements/tiy/conditional_tests.py
+++ 2020/05_if statements/tiy/conditional_tests.py
@@ -14,19 +14,51 @@ print(car == 'audi')
 # Create at least ten tests. Have at least five tests evaluate TRUE and another five that evaluate to FALSE.
 
 print("\n\tTest One")
-new_car = 'truck'
-print("\nWill my new_car == 'truck'? I predict True.")
-print(new_car == 'truck')
-
-print("Will my new_car == 'suv'? I predict False.")
-print(new_car == 'suv')
+favorite_pizza = 'pepperoni'
+print("\nIs my favorite_pizza == 'pepperoni'? I predict True.")
+print(favorite_pizza == 'pepperoni')
 
 print("\n\tTest Two")
+old_car = 'subaru'
+print("\nIs my old_car == 'subaru'? I predict True.")
+print(old_car == 'subaru')
+
 print("\n\tTest Three")
+new_car = 'bronco'
+print("\nWill my new_car == 'bronco'? I predict True.")
+print(new_car == 'bronco')
+
 print("\n\tTest Four")
+callie = 'naked'
+print("\nIs callie == 'naked'? I predict True.")
+print(callie == 'naked')
+
 print("\n\tTest Five")
+otis = 'pug'
+print("\nIs otis != 'german shephard'? I predict True.")
+print(otis != 'german shephard')
+
 print("\n\tTest Six")
+callie = 'lovely'
+print("\nIs callie == 'mean'? I predict False. She is lovely.")
+print(callie == 'mean')
+
 print("\n\tTest Seven")
+otis = 'pug'
+print("\nIs Otis a chihuahua? I think False. He is a Pug.")
+print(otis == 'chihuahua')
+
 print("\n\tTest Eight")
+callie = 'clothed'
+print("\nIs Callie still 'naked'? I think False, she has clothes on.")
+print(callie == 'naked')
+
 print("\n\tTest Nine")
+current_favorite_car = 'bronco sport'
+print("\nIs my favorite car still a 'jeep'? False, right now I want a Bronco.")
+print(current_favorite_car == 'jeep')
+
 print("\n\tTest Ten")
+next_job = 'technical'
+print("\nWill my next job be recruiting? I think False. I want to be technical.")
+print(next_job == 'recruiting')
