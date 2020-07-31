5e590ce7682bcfb788bed5f7a6881db3d1e1d04f
node4thewin <node4thewin@users.noreply.github.com>
2020-07-31 16:41:40 -0400

    updated 7/31

---
+++ 2020/06_dictionaries/try it yourself/polling.py
@@ -0,0 +1,6 @@
+# 6-6 POLLING: Use the code in 'favorite_languages.py'
+
+# Make a list of people who should take the favorite languages poll. Include some names that are already in the dictionary and some that are not.
+
+# Loop through a list of people who should take the poll. If they have already taken the poll, print a message thanking them for responding. If they have not yet taken the poll, print a message inviting them to take the poll.
+
--- 2020/06_dictionaries/try it yourself/rivers.py
+++ 2020/06_dictionaries/try it yourself/rivers.py
@@ -1,9 +1,24 @@
 # 6-5 RIVERS
 
-# Make a dictionary containing three major rivers and the country each river runs through. One key-value pair might be 'nile': 'egypt'.
+# Make a dictionary containing three major rivers and the state/country each river runs through. One key-value pair might be 'nile': 'egypt'.
+
+rivers = {
+  'nile': 'egypt',
+  'shenandoah': 'virginia',
+  'colorado': 'colorado',
+  'potomac': 'virginia'
+}
 
 # Use a loop to print a sentence about each river, such as the "The Nile runs through Egypt".
+for river, location in rivers.items():
+  print(f"The {river.title()} River runs through {location.title()}")
 
 # Use a loop to print the name of each river included in the dictionary.
+print("\nHere is a list of all the Rivers:\n")
+for river in rivers.keys():
+  print(river.title())
 
 # Use a loop to print th ename of each country included in the dictionary.
+print("\nHere is a list of all the Locations:\n")
+for river in set(rivers.values()):
+  print(river.title())
