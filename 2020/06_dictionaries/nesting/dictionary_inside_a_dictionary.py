06e1a23bd2c9770baf0ff72440b26bf0fa77a025
node4thewin <node4thewin@users.noreply.github.com>
2020-08-05 13:36:33 -0400

    updated dictionary

--- 2020/06_dictionaries/nesting/dictionary_inside_a_dictionary.py
+++ 2020/06_dictionaries/nesting/dictionary_inside_a_dictionary.py
@@ -8,4 +8,25 @@ users = {
     'last': 'einstein',
     'location': 'princeton',
   },
+
+  'mcurie': {
+    'first': 'marie',
+    'last': 'curie',
+    'location': 'paris',
+  },
+
 }
+
+for username, user_info in users.items():
+  print(f"\nUsername: {username}")
+  full_name = f"{user_info['first']} {user_info['last']}"
+  location = user_info['location']
+
+  print(f"\tFull name: {full_name.title()}")
+  print(f"\tLocation: {location.title()}")
+
+# We first define a dictionary called 'users' with two keys: one each for usernames 'aeinstein' and 'mcurie'. The value associated with each key is a dictionary that includes the user's first name, last name, and location. At line 20 we loop through each 'users' dictionary. Python assigns each key to the variable 'username', and the dictionary associated with each username is assigned to the variable 'user_info'. Once inside the main dictionary loop, we print the username at line 21. 
+
+# At line 23 we start accessing the inner dictionary. The variable 'user_info' which contains the dictionary of user information, has three keys: 'first', 'last', and 'location'. We use each key to generate a neatly formatted full name and location for each person, and then print a summary of what we know about each user (line 25).
+
+# Notice that the structure of each user's dictionary is identical. Although not required by Python, this structure makes nested dictionaries easier to work with. If each user's dictionary had different keys, the code inside the 'for' loop would be more complicated.
---
+++ 2020/06_dictionaries/try it yourself/people.py
@@ -0,0 +1,29 @@
+# 6-7 PEOPLE
+
+# Start with the program you wrote for Exercise 6-1 (page 99). Make two new dictionaries representing different people, and store all three dictionaries in a list called 'people'. Loop through your list of people. As you loop through the list, print everything you know about each person.
+
+people = {
+  'biebrh0le69': {
+    'first': 'callie',
+    'last': 'barron',
+    'relationship': 'girlfriend',
+    'age': '27',
+    'location': 'herndon',
+  },
+  'mgharvest': {
+    'first': 'michael',
+    'last': 'harvey',
+    'relationship': 'brother',
+    'age': '27',
+    'location': 'sterling',
+  },
+  'mischiefmngd': {
+    'first': 'erika',
+    'last': 'gerard',
+    'relationship': 'sister',
+    'age': '31',
+    'location': 'purcellville',
+  },
+
+}
+
