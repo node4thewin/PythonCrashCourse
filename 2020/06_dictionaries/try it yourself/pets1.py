6606096d61c1237d50e2c134390b63fc69fcd9d7
node4thewin <node4thewin@users.noreply.github.com>
2020-08-08 09:39:30 -0400

    pets1

---
+++ 2020/06_dictionaries/try it yourself/pets1.py
@@ -0,0 +1,79 @@
+pets = []
+
+pet = {
+  'name': 'otis',
+  'owner': 'callie',
+  'breed': 'pug'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'lola',
+  'owner': 'andrew',
+  'breed': 'golden retriever'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'ruth',
+  'owner': 'penny',
+  'breed': 'labrador retriever'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'apollo',
+  'owner': 'daniel',
+  'breed': 'german shephard'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'spencer',
+  'owner': 'james',
+  'breed': 'springer spaniel'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'chai',
+  'owner': 'xenia',
+  'breed': 'mini golden doodle'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'emma',
+  'owner': 'erika',
+  'breed': 'mix'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'zorro',
+  'owner': 'mike sr.',
+  'breed': 'shitzu'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'hesh',
+  'owner': 'mike sr.',
+  'breed': 'miniature schnauzer'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'joey',
+  'owner': 'susan',
+  'breed': 'mini shitzu'
+}
+pets.append(pet)
+
+for pet in pets:
+  name = f"{pet['name'].title()}"
+  owner = f"{pet['owner'].title()}"
+  breed = f"{pet['breed'].title()}"
+
+  print(f"{name} is a {breed} owned by {owner}!")
+
