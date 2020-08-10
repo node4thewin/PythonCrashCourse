78c5e5b1fe63ba8f315ccecac759799209a2aad6
node4thewin <node4thewin@users.noreply.github.com>
2020-08-10 18:23:41 -0400

    people/pets from desktop

--- 2020/06_dictionaries/try it yourself/people.py
+++ 2020/06_dictionaries/try it yourself/people.py
@@ -2,7 +2,9 @@
 
 # Start with the program you wrote for Exercise 6-1 (page 99). Make two new dictionaries representing different people, and store all three dictionaries in a list called 'people'. Loop through your list of people. As you loop through the list, print everything you know about each person.
 
-people = {
+# I adjusted people to usernames...you can see the solution at the bottom of this program.
+
+usernames = {
   'biebrh0le69': {
     'first': 'callie',
     'last': 'barron',
@@ -27,3 +29,51 @@ people = {
 
 }
 
+for username, username_info in usernames.items():
+  print(f"\nUsername: {username}")
+  full_name = f"{username_info['first']} {username_info['last']}"
+  relation = username_info['relationship']
+  age = username_info['age']
+  location = username_info['location']
+
+  print(f"\tFull name: {full_name.title()}")
+  print(f"\tRelation: {relation.title()}")
+  print(f"\tAge: {age.title()}")
+  print(f"\tLocation: {location.title()}")
+
+# this is the version from SOLUTIONS:
+
+people = []
+
+person = {
+  'first_name': 'eric',
+  'last_name': 'matthes',
+  'age': 46,
+  'city': 'sitka',
+}
+people.append(person)
+
+person = {
+  'first_name': 'lemmy',
+  'last_name': 'matthes',
+  'age': 2,
+  'city': 'sitka',
+}
+people.append(person)
+
+person = {
+  'first_name': 'willie',
+  'last_name': 'matthes',
+  'age': 11,
+  'city': 'sitka',
+}
+people.append(person)
+
+# Display all of the information in the dictionary.
+for person in people:
+  name = f"{person['first_name'].title()} {person['last_name'].title()}"
+  age = person['age']
+  city = person['city'].title()
+
+  print(f"{name}, of {city}, is {age} years old.")
+  # make sure this print() call is a part of the for loop.
---
+++ 2020/06_dictionaries/try it yourself/pets.py
@@ -0,0 +1,52 @@
+# 6-8 PETS
+
+# Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the owner's name. Store these dictionaries in a list called 'pets'. Next, loop through your list and as you do, print everything you know about each pet.
+
+pets = []
+
+pet = {
+  'name': 'otis',
+  'owner': 'callie',
+  'breed': 'white pug',
+}
+pets.append(pet)
+
+pet = {
+  'name': 'spencer',
+  'owner': 'james',
+  'breed': 'white pug',
+}
+pets.append(pet)
+
+pet = {
+  'name': 'lola',
+  'owner': 'andrew',
+  'breed': 'golden retriever',
+}
+pets.append(pet)
+
+pet = {
+  'name': 'apollo',
+  'owner': 'daniel gerard',
+  'breed': 'german shephard',
+}
+pets.append(pet)
+
+pet = {
+  'name': 'emma',
+  'owner': 'erika gerard',
+  'breed': 'mix',
+}
+pets.append(pet)
+
+pet = {
+  'name': 'chai',
+  'owner': 'xenia',
+  'breed': 'mini golden doodle',
+}
+pets.append(pet)
+
+for pet in pets:
+  name = f"{pet['name'].title()}"
+  age = f"{pet['owner']}"
+  breed = f"{pet['breed']}"
