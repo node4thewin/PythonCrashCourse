00889437f90feb0677b6c84b6a2b6ecc967d29f4
node4thewin <node4thewin@users.noreply.github.com>
2020-08-16 10:50:14 -0400

    updated

--- 2020/08_functions/passing arguments/multiple_function_calls.py
+++ 2020/08_functions/passing arguments/multiple_function_calls.py
@@ -11,5 +11,12 @@ describe_pet('hamster', 'harry')
 describe_pet('dog', 'otis')
 describe_pet('dog', 'lola')
 
-# In this second function call, we pass 'describe_pet()' the arguments 'dog' and 'otis'. As with the previous set of arguments we used, Python matches 'dog' with the parameter 'animal_type' and 'willie' with the parameter 'pet_name'.
+# In this second function call, we pass 'describe_pet()' the arguments 'dog' and 'otis'. As with the previous set of arguments we used, Python matches 'dog' with the parameter 'animal_type' and 'otis' with the parameter 'pet_name'.
 
+# As before, the function does its job, but this time it prints values for a dog named Otis. Now we have a dog named Otis and another dog named Lola.
+
+# Calling a function multiple times is a very efficient way to work. The code describing a pet is written once in the function. Then, anytime you want to describe a new pet, you call the function with the new pet's information. Even if the code for describing a pet were to expand to ten lines, you could still describe a new pet in just one line by calling the function again.
+
+# You can use as many positional arguments as you need in your functions. Python works through the arguments you provide when calling the function and matches each one with the corresponding parameter in the function's definition.
+
+# You can use as many positional arguments as you need in your functions. Python works through the arguments you provide when calling the function and matches each one with the corresponding parameter in the function's definition.
