42275f58cb1b911f316e9f489172550f583c6399
node4thewin <node4thewin@users.noreply.github.com>
2020-09-01 15:08:48 -0400

    9.1

--- 2020/09_classes/inheritance/the_init_method_for_a_child_class.py
+++ 2020/09_classes/inheritance/the_init_method_for_a_child_class.py
@@ -40,3 +40,13 @@ class ElectricCar(Car):
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
+
+# At line 9 we start with Car. When you create a child class, the parent class must be part of the current file and must appear before the child class in the file. At line 34 we define the child class, 'ElectricCar'. The name of the parent class must be included in parentheses in the definition of a child class. The __init()__ method at line 37 takes in the information required to make a 'Car' instance.
+
+# The 'super()' function at line 39 is a special function that allows you to call a method from the parent class. This line tells Python to call the __init__() method from 'Car', which gives an 'ElectricCar' instance all the attributes defined in that method. The name 'super' comes from a convention of calling the parent class a 'superclass' and the child class a 'subclass'.
+
+# We test whether inheritance is working properly by trying to create an electric car with the same kind of information we'd provide when making a regular car. At line 41 we make an instance of the 'ElectricCar' class and assign it to 'my_tesla'. This line calls the __init__() method defined in the parent class 'Car'. We provide the arguments 'tesla', 'model s', and 2019.
+
+# Aside from __init__(), there are no attributes or methods yet that are particular to an electriv car. At this point we're just making sure the electric car has an appropriate Car behaviors.
+
+# The 'ElectricCar' instance works just like an instance of 'Car', so now we can begin defining attributes and methods specific to electric cars.
