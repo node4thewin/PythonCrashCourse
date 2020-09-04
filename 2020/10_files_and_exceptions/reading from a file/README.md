0c8e12eab3eebec7e6bef816ad8bc4ac270bb8e1
node4thewin <37819597+node4thewin@users.noreply.github.com>
2020-09-04 14:13:46 -0400

    9.4

---
+++ 2020/10_files_and_exceptions/reading from a file/README.md
@@ -0,0 +1,5 @@
+# Reading from a File
+
+An incredible amount of data is available in text files. Text files can contain weather data, traffic data, socioeconomic data, literary works, and more. Reading from a file is particularly useful in data analysis applications, but it's also applicable to any situation in which you wnat to analyze or modify information stored in a file. For example, you can write a program that reads in the contexts of a text file. For example, you can write a program that reads in the contents of a text file and rewrites the file with formatting that allows a browser to display it.
+
+When you want to work with the information in a text file, the first step is to read the file into memory. You can read the entire contents of a file, or you can work through the file one line at a time.
---
+++ 2020/10_files_and_exceptions/reading from a file/reading_an_entire_file.py
@@ -0,0 +1,19 @@
+# Reading an Entire File
+
+# To begin, we need a file with a few lines of text in it. Let's start with a file that contains 'pi' to 30 decimal places, with 10 decimale places per line:
+
+with open('pi_digits.txt') as file_object:
+  contents = file_object.read()
+print(contents)
+
+# The first line of this program has a lot going on. Let's start by looking at the 'open()' function. To do any work with a file, even just printing its contents, you first need to 'open' the file to access it. The 'open()' function needs one argument: the name of the file you want to open. Python looks for this file in the directory where the program that's currently being executed is stored. In this example, 'file_reader.py' is currently running, so Python looks for 'pi_digits.txt' in the directory where 'file_reader.py' is stored. The open() function returns an object representing the file. Here, 'open('pi_digits.txt') returns an object representing 'pi_digits.txt'. Python assings this object to 'file_object', which we'll work with later in this program.
+
+# The keyword 'with' closes the file once access to it is no longer needed. Notice how we call 'open()' in this program but not 'close()'. You could open and close the file by calling 'open()' and 'close()', but if a bug in your program prevents the close() method from being executed, the file may never close. This may seem trivial, but improperly closed files can cause data to be lost or corrupted. And if you call 'close()' too early in your program, you'll find yourself trying to work with a 'closed' file (a file you can't access), which leads to more errors. It's not always easy to know exactly when you should close a file, but with the structure shown here, Python will figure that out for you. All you have to do is open the file and work with it as desired, trusting that Python will close it automatically when the 'with' block finishes execution.
+
+# Once we have a file object representing 'pi_digits.txt', we use the 'read()' method in the second line of our program to read the entire contents of the file and store it as one long string in 'contents'. When we print the value of 'contents' we get the entire text file back...
+
+# The only difference between this output and the original file is the extra blank line at the end of the output. The blank line appears because 'read()' returns an empty string when it reaches the end of the file; this empty string shows up as a blank line. If you want to remote the extra blank line, you can use 'rstrip()' in the call to print():
+
+with open('pi_digits.txt') as file_object:
+  contents = file_object.read()
+  print(contents.rstrip())
