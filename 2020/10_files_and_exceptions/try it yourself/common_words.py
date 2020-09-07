67553d4348bf370b55cf1d3869b04645ba94b80e
node4thewin <37819597+node4thewin@users.noreply.github.com>
2020-09-07 09:32:18 -0400

    9.7

--- 2020/10_files_and_exceptions/try it yourself/common_words.py
+++ 2020/10_files_and_exceptions/try it yourself/common_words.py
@@ -12,7 +12,24 @@
 
 # Write a program that reads the files you found at Project Gutenberg and determines how many times the word 'the' appears in each text. This will be an approximation because it will also count words such as 'then' and 'there'. Try counting 'the ', with a space in that string, and see how much lower your count is.
 
-filenames = ['minions_of_the_crystal_sphere.txt', 'uncle_toms_cabin.txt', 'memoirs_of_us_grant.txt']
+def count_common_words(filename, word):
+    """Count how many times word appears in the text."""
+    # Note: This is a really simple approximation, and the number returned
+    #   will be higher than the actual count.
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+        word_count = contents.lower().count(word)
 
+        msg = f"'{word}' appears in {filename} about {word_count} times."
+        print(msg)
 
+filename = 'minions_of_the_crystal_sphere.txt'
+count_common_words(filename, 'the ')
+
+filename = 'uncle_toms_cabin.txt'
+count_common_words(filename, 'the ')
 
