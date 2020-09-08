8f3ad61fb27919c203471bf8463af934cecb3913
node4thewin <37819597+node4thewin@users.noreply.github.com>
2020-09-07 21:55:37 -0400

    9.7

--- 2020/10_files_and_exceptions/storing data/refactoring.py
+++ 2020/10_files_and_exceptions/storing data/refactoring.py
@@ -26,6 +26,39 @@
 
 # Let's refactor 'greet_user()' so it's not doing so many different tasks. We'll start by moving the code for retrieving a stored username to a separate function:
 
+# import json
+
+# def get_stored_username():
+#   """Get stored username if available."""
+#   filename = 'username.json'
+#   try:
+#     with open(filename) as f:
+#       username = json.load(f)
+#   except FileNotFoundError:
+#     return None
+#   else:
+#     return username
+
+# import json
+
+# def greet_user():
+#   """Greet the user by name."""
+#   username = get_stored_username()
+#   if username:
+#     print(f"Welcome back, {username}!")
+#   else:
+#     username = input("What is your name: ")
+#     filename = 'username.json'
+#     with open(filename, 'w') as f:
+#       json.dump(username, f)
+#       print(f"We'll remember you when you come back, {username}!")
+
+# greet_user()
+
+# The new function 'get_stored_username()' has clear purpose, as stated in the docstring (line 32). This function retrieves a stored username and returns the username if it finds one. If the file 'username.json' doesn't exist, the function returns 'None' (line 38). This is good practice: a function should either return the value you're expecting, or it should return 'None'. This allows us to perform a single test with the return value of the function. At line 47 we print a welcome back message to the user if the attempt to retrieve a username was successful, and if it doesn't, we prompt for a new username.
+
+# We should factor one more black of code out of 'greet_user()' If the username doesn't exist, we should move the code that prompts for a new username to a function dedicated to that purpose:
+
 import json
 
 def get_stored_username():
@@ -39,7 +72,13 @@ def get_stored_username():
   else:
     return username
 
-import json
+def get_new_username():
+  """Prompt for a new username."""
+  username = input("What is your name? ")
+  filename = 'username.json'
+  with open(filename, 'w') as f:
+    json.dump(username, f)
+    return username
 
 def greet_user():
   """Greet the user by name."""
@@ -47,14 +86,9 @@ def greet_user():
   if username:
     print(f"Welcome back, {username}!")
   else:
-    username = input("What is your name: ")
-    filename = 'username.json'
-    with open(filename, 'w') as f:
-      json.dump(username, f)
-      print(f"We'll remember you when you come back, {username}!")
+    username = get_new_username()
+    print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
 
-# The new function 'get_stored_username()' has clear purpose, as stated in the docstring (line 32). This function retrieves a stored username and returns the username if it finds one. If the file 'username.json' doesn't exist, the function returns 'None' (line 38). This is good practice: a function should either return the value you're expecting, or it should return 'None'. This allows us to perform a single test with the return value of the function. At line 47 we print a welcome back message to the user if the attempt to retrieve a username was successful, and if it doesn't, we prompt for a new username.
-
-# We should factor one more black of code out of 'greet_user()' If the username doesn't exist, we should move the code that prompts for a new username to a function dedicated to that purpose:
+# Each function in this final version of 'remember_me.py' has a single, clear purpose. We call 'greet_user()', and that function prints an appropriate message: it either welcomes back an existing user or greets a new user. It does this by calling 'get_stored_username()', which is responsible only for retrieving a stored username if one exists. Finally, 'greet_user()' calls 'get_new_username()' if necessary, which is responsible only for getting a new username and storing it. This compartmentalization of work is an essential part of writing clear code that will be easy to maintain and extend.
---
+++ 2020/10_files_and_exceptions/try it yourself/favorite_number.py
@@ -0,0 +1,15 @@
+# 10-11 Favorite Number
+
+# Write a program that promps for the user's favorite number. Use json.dump() to store this number in a file. Write a separate program that reads in this value and prints the message, "I know your favorite number! It's _____."
+
+import json
+
+fav_num = input("What's your favorite number? ")
+
+filename = 'fav_num.json'
+with open(filename, 'w') as f:
+  json.dump(fav_num, f)
+
+with open(filename) as f:
+  fav_num = json.load(f)
+  print(f"I know your favorite number! It's {fav_num}.")
